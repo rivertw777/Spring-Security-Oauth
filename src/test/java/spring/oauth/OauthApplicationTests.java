cd7c3f854d64a7cd218e9b834c4978dacb9ed8b4
rivertw777 <105557972+rivertw777@users.noreply.github.com>
2024-04-24 18:28:52 +0900

    initial commit

---
+++ src/test/java/spring/oauth/OauthApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
