7ea960b1035e6f7518c851e4ce16260d18b9ef91
rivertw777 <105557972+rivertw777@users.noreply.github.com>
2024-04-27 23:20:54 +0900

    feat: Form 로그인 구현

--- src/main/java/spring/oauth/config/SecurityConfig.java
+++ src/main/java/spring/oauth/config/SecurityConfig.java
@@ -5,32 +5,48 @@ import static org.springframework.security.web.util.matcher.AntPathRequestMatche
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import spring.oauth.config.oauth.CustomOauth2UserService;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private CustomOauth2UserService customOauth2UserService;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .sessionManagement(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authz) -> authz
-                        // 회원 가입
-                        .requestMatchers(antMatcher(HttpMethod.POST, "/api/users")).permitAll()
-                        .anyRequest().authenticated()
+                        .requestMatchers(antMatcher("/user/**")).authenticated()
+                        .anyRequest().permitAll()
+                )
+                // Form 로그인
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/loginProc")
+                .successHandler(authenticationSuccessHandler())
+                .and()
+                // Oauth2 로그인
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/login")
                 );
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler authenticationSuccessHandler() {
+        return (request, response, authentication) -> {
+            response.sendRedirect("/");
+        };
     }
 
     @Bean
