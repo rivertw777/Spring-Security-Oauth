781cc75a62ae5f82af139c92f6ab339a74dcb60a
rivertw777 <105557972+rivertw777@users.noreply.github.com>
2024-05-02 23:44:53 +0900

    feat: oauth2 인증 성공 핸들러 구현

--- src/main/java/spring/oauth/config/SecurityConfig.java
+++ src/main/java/spring/oauth/config/SecurityConfig.java
@@ -1,18 +1,28 @@
 package spring.oauth.config;
 
+import static org.antlr.v4.runtime.atn.SemanticContext.and;
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import spring.oauth.config.jwt.JwtAuthorizationFilter;
 import spring.oauth.config.oauth.CustomOauth2UserService;
+import spring.oauth.config.oauth.OAuth2LoginSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -20,6 +30,8 @@ import spring.oauth.config.oauth.CustomOauth2UserService;
 public class SecurityConfig {
 
     private final CustomOauth2UserService customOauth2UserService;
+    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -27,8 +39,18 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authz) -> authz
                         .requestMatchers(antMatcher("/user/**")).authenticated()
+                        .requestMatchers(antMatcher("/api/test")).authenticated()
                         .anyRequest().permitAll()
                 )
+                .exceptionHandling()
+                .accessDeniedHandler(new AccessDeniedHandler() {
+                    @Override
+                    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+                        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                        response.getWriter().write("Access Denied");
+                    }
+                })
+                .and()
                 // Form 로그인
                 .formLogin()
                 .loginPage("/login")
@@ -37,10 +59,14 @@ public class SecurityConfig {
                 .and()
                 // Oauth2 로그인
                 .oauth2Login()
-                .loginPage("/login")
-                .successHandler(authenticationSuccessHandler())
+                .loginPage("/login/social")
+                .successHandler(oAuth2LoginSuccessHandler)
                 .userInfoEndpoint()
-                .userService(customOauth2UserService);
+                .userService(customOauth2UserService)
+                // 인가 필터 추가
+                .and()
+                .and()
+                .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
---
+++ src/main/java/spring/oauth/config/oauth/OAuth2LoginSuccessHandler.java
@@ -0,0 +1,52 @@
+package spring.oauth.config.oauth;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URI;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.util.UriComponentsBuilder;
+import spring.oauth.config.auth.CustomUserDetails;
+import spring.oauth.config.jwt.TokenProvider;
+
+@Component
+@Transactional
+@RequiredArgsConstructor
+public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final TokenProvider tokenProvider;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        String accessToken = tokenProvider.generateToken(userDetails);
+        String uri = createURI(accessToken).toString();
+        getRedirectStrategy().sendRedirect(request, response, uri);
+    }
+
+    // Redirect URI 생성. JWT를 쿼리 파라미터로 담아 전달한다.
+    private URI createURI(String accessToken) {
+        MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+        queryParams.add("access_token", accessToken);
+        return UriComponentsBuilder
+                .newInstance()
+                .scheme("http")
+                .host("localhost")
+                .port(8080)
+                .path("/")
+                .queryParams(queryParams)
+                .build()
+                .toUri();
+    }
+
+}
